22508290b3e032a7d6c992d0f37c71d5156f154b
darwady2 <darwady2@gmail.com>
2017-12-26 16:50:40 -0600

    starting to test 1.3

--- 13pythonStudy.py
+++ 13pythonStudy.py
@@ -8,3 +8,13 @@ Steps:
 2. Get the length
 3. Replace s[i] with %20 if == "_"
 """
+def url(s):
+    s = s.Trim()
+    return s
+
+def main():
+    R = url(s = 'dan   ')
+    print R
+
+if __name__ == "__main__":
+    main()
