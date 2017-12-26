a3dd577657ffb2d145487cc677d92e0df4aa304f
darwady2 <darwady2@gmail.com>
2017-12-26 17:06:08 -0600

    finished 1.3

--- 13pythonStudy.py
+++ 13pythonStudy.py
@@ -6,14 +6,20 @@ Output: Mr%20John%20Smith"
 Steps:
 1. Trim the string
 2. Get the length
-3. Replace s[i] with %20 if == "_"
+3. Replace s[i] with %20 if == "_".
+ -- Do this with a list, since strings are immutable in Python and we have to iterate through at least once to find the spaces.
 """
+
 def url(s):
-    s = s.Trim()
-    return s
+    sList = list(s.strip())
+    for i, j in enumerate(sList):
+        if j == " ":
+            sList[i] = "%20"
+    sList = ''.join(sList)
+    return sList
 
 def main():
-    R = url(s = 'dan   ')
+    R = url(s = 'dan is neat  ')
     print R
 
 if __name__ == "__main__":
