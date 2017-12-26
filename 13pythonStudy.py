b16134d7e55e02499a6484db440196772a84116c
darwady2 <darwady2@gmail.com>
2017-12-26 16:46:53 -0600

    Working on 1.3

---
+++ 13pythonStudy.py
@@ -0,0 +1,10 @@
+"""
+Replace all spaces in a string with "%20", i.e.
+Input: "Mr. John Smith"
+Output: Mr%20John%20Smith"
+
+Steps:
+1. Trim the string
+2. Get the length
+3. Replace s[i] with %20 if == "_"
+"""
